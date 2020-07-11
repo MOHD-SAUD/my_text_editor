d13ef29bc8cf495937815377f67bb8343325bd72
Mohd Saud <mohdsaud2010@gmail.com>
2020-07-12 00:51:45 +0530

    Improvised the app

--- server.js
+++
@@ -1,6 +0,0 @@
-const express=require('express');
-const server=express();
-server.use('/',express.static(__dirname+'/public'));
-server.listen(2121,()=>{
-    console.log("Server started at http://localhost:2121");
-})
